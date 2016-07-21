f3ebf9cbf18e821a042e55e635178913ea2e639d
loonggg <loonggg@qq.com>
2016-07-21 19:16:51 +0800

    TextInputLayout和TextInputEditText相关知识

--- app/src/main/java/com/loonggg/materialdesigndemo/StyleActivity.java
+++ app/src/main/java/com/loonggg/materialdesigndemo/StyleActivity.java
@@ -1,12 +1,22 @@
 package com.loonggg.materialdesigndemo;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class StyleActivity extends BaseActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_style);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/loonggg/materialdesigndemo/ToolbarActivity.java
+++ app/src/main/java/com/loonggg/materialdesigndemo/ToolbarActivity.java
@@ -1,13 +1,23 @@
 package com.loonggg.materialdesigndemo;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class ToolbarActivity extends BaseActivity {
     private Toolbar toolbar;
+    private TextInputLayout email_textlayout;
+    private EditText email_et;
+    private TextInputEditText pwdEt;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,33 @@ public class ToolbarActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btn = (Button) findViewById(R.id.btn);
+
+        email_textlayout = (TextInputLayout) findViewById(R.id.email_textlayout);
+        email_et = (EditText) findViewById(R.id.email_et);
+
+        pwdEt = (TextInputEditText) findViewById(R.id.pwd_et);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!email_et.getText().toString().equals("loonggg@qq.com")) {
+                    //设置错误信息可用
+                    email_textlayout.setErrorEnabled(true);
+                    //设置错误信息
+                    email_textlayout.setError("邮箱不正确");
+                } else {
+                    email_textlayout.setErrorEnabled(false);
+                }
+                if (pwdEt.getText().toString().equals("000000")) {
+
+                } else {
+                    pwdEt.setError("密码不正确");
+                }
             }
         });
     }
